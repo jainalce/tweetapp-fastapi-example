abfd7534aa0867ff13f08ae90ba51ba3acecc0dc
Jainal <jainalce@gmail.com>
2022-03-12 18:31:50 -0500

    intital commit to all files

---
+++ alembic/versions/56a2199b3a5b_create_posts_table.py
@@ -0,0 +1,23 @@
+"""create posts table
+
+Revision ID: 56a2199b3a5b
+Revises: 
+Create Date: 2022-03-12 16:52:14.197796
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '56a2199b3a5b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
